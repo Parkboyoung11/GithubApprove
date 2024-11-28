f4af0b8b33fd3e86309664e50a56d73c10ebe6f1
akii <vuhongsonjv11@gmail.com>
2024-11-28 23:56:33 +0900

    refactor

--- content.js
+++ content.js
@@ -1,14 +1,16 @@
-function addLGTMButton() {
+function addApproveButton() {
   const reviewChangeButton = document.querySelector("#overlay-show-review-changes-modal");
-  if (reviewChangeButton && !document.querySelector("#custom-approve-btn")) {
-    const newButton = document.createElement("button");
-    newButton.id = "custom-approve-btn";
-    newButton.textContent = "LGTM Approve & Submit";
-    newButton.className = "Button--primary Button--small Button";
-    newButton.style.marginLeft = "10px";
-    reviewChangeButton.parentNode.insertBefore(newButton, reviewChangeButton.nextSibling);
 
-    newButton.addEventListener("click", function () {
+  if (reviewChangeButton && !document.querySelector("#custom-approve-btn")) {
+    const approveButton = document.createElement("button");
+    approveButton.id = "custom-approve-btn";
+    approveButton.textContent = "LGTM Approve & Submit";
+    approveButton.className = "Button--primary Button--small Button";
+    approveButton.style.marginLeft = "10px";
+
+    reviewChangeButton.parentNode.insertBefore(approveButton, reviewChangeButton.nextSibling);
+
+    approveButton.addEventListener("click", function () {
       const form = document.querySelector("#pull_requests_submit_review");
       const approveInput = document.querySelector("#pull_request_review\\[event\\]_approve");
       const textarea = document.querySelector("#pull_request_review_body");
@@ -17,32 +19,17 @@ function addLGTMButton() {
         approveInput.checked = true;
         textarea.value = "LGTMです！";
         form.submit();
-      } else {
-        console.error("Form, input, or textarea not found!");
       }
     });
-  } else {
-    console.error("Review change button not found!");
   }
 }
 
-const observer = new MutationObserver((mutations) => {
-  let relevantChange = false;
-
-  mutations.forEach((mutation) => {
-    if (
-      mutation.addedNodes.length > 0 &&
-      document.querySelector("#overlay-show-review-changes-modal")
-    ) {
-      relevantChange = true;
-    }
-  });
-
-  if (relevantChange) {
-    addLGTMButton();
+const observer = new MutationObserver(() => {
+  if (document.querySelector("#overlay-show-review-changes-modal")) {
+    addApproveButton();
   }
 });
 
 observer.observe(document.body, { childList: true, subtree: true });
 
-addLGTMButton();
+addApproveButton();
